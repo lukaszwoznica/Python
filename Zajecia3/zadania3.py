49f8f85cc095e1828825ad888aae0fe48e2342e7
lukaszwoznica <31970640+lukaszwoznica@users.noreply.github.com>
2018-03-14 13:53:45 +0100

    Update zadania3.py

--- Zajecia3/zadania3.py
+++ Zajecia3/zadania3.py
@@ -55,3 +55,33 @@ print(a/b)
 print(a.mod())
 print(b.mod())
 print(a==b)
+
+
+#Zadanie 2
+
+
+class Punkt2D(object):
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    
+  def odleglosc(self, second):
+    dist = ((second.x - self.x)**2 + (second.y - self.y)**2)**0.5
+    return dist
+      
+class Punkt3D(Punkt2D):
+  def __init__(self, x, y, z):
+    super().__init__(x, y)
+    self.z = z
+    
+  def odleglosc(self, second):
+    dist = ((second.x-self.x)**2 + (second.y-self.y)**2 + (second.z-self.z)**2)**0.5
+    return dist
+    
+p2d_1 = Punkt2D(0,0)
+p2d_2 = Punkt2D(2,2)
+p3d_1 = Punkt3D(0,0,0)
+p3d_2 = Punkt3D(2,2,2)
+print(p2d_1.odleglosc(p2d_2))
+print(p3d_1.odleglosc(p3d_2))
+ 

5eef7e958a6bf5ef22e744f5c522eeb2d197f358
lukaszwoznica <31970640+lukaszwoznica@users.noreply.github.com>
2018-03-21 00:59:30 +0100

    Update zadania3.py

--- Zajecia3/zadania3.py
+++ Zajecia3/zadania3.py
@@ -85,3 +85,100 @@ p3d_2 = Punkt3D(3, 3, 3)
 print(p2d_1.odleglosc(p2d_2))
 print(p3d_1.odleglosc(p3d_2))
  
+# Zadanie 3
+
+class Kolo(object):
+  __rozmiar = None
+  __markaOpony = None
+  
+  def __init__(self, rozmiar, markaOpony):
+    self.__rozmiar = rozmiar
+    self.__markaOpony = markaOpony
+
+  def setRozmiar(self, rozmiar):
+    self.__rozmiar = rozmiar
+  
+  def setMarkaOpony(self, markaOpony):
+    self.__markaOpony = markaOpony
+  
+  def getRozmiar(self):
+    return self.__rozmiar
+  
+  def getMarkaOpony(self):
+    return self.__markaOpony
+    
+  def __str__(self):
+    ret = "Kolo: \nRozmiar: "+str(self.__rozmiar)+"\nMarka opony: "+str(self.__markaOpony)
+    return ret
+    
+class Silnik(object):
+  __pojemnosc = None
+  __moc = None
+  
+  def __init__(self, pojemnosc, moc):
+    self.__pojemnosc = pojemnosc
+    self.__moc = moc 
+  
+  def setPojemnosc(self, pojemnosc):
+    self.__pojemnosc = pojemnosc
+  
+  def setMoc(self, moc):
+    self.__moc = moc
+  
+  def getPojemnosc(self):
+    return self.__pojemnosc
+  
+  def getMoc(self):
+    return self.__moc
+    
+  def __str__(self):
+    ret = "Silnik: \nPojemnosc: "+str(self.__pojemnosc)+"\nMoc: "+str(self.__moc)
+    return ret
+          
+class ZbiornikPaliwa(object):
+  __maxPojemnosc = None
+  __obecnaIlosc = None
+    
+  def __init__(self, maxPojemnosc):
+    self.__maxPojemnosc = maxPojemnosc
+   
+  def setObecnaIlosc(self, obecnaIlosc):
+    self.__obecnaIlosc = obecnaIlosc     
+
+  def getObecnaIlosc(self):
+    return self.__obecnaIlosc
+    
+  def __str__(self):
+    ret = "Zbiornik: \nMax ilosc paliwa: "+str(self.__maxPojemnosc)+"\nObecna ilosc paliwa: "+str(self.__obecnaIlosc)
+    return ret
+    
+class Samochod(Kolo, Silnik, ZbiornikPaliwa):
+  __marka = None
+  __model = None
+    
+  def __init__(self, marka, model, rozmiarKola, markaOpony, pojemnosc, moc, pojemnoscZbiornika):
+    Kolo.__init__(self, rozmiarKola, markaOpony)
+    Silnik.__init__(self, pojemnosc, moc)
+    ZbiornikPaliwa.__init__(self, pojemnoscZbiornika)
+    self.__marka = marka
+    self.__model = model
+    
+  def setMarka(self, marka):
+    self.__marka = marka
+  
+  def setModel(self, model):
+    self.__model = model
+  
+  def getMarka(self):
+    return self.__marka
+  
+  def getModel(self):
+    return self.__model
+    
+  def __str__(self):
+    ret = "Samochod:\nMarka: "+str(self.__marka)+"\nModel: "+str(self.__model)
+    return ret+"\n" + Silnik.__str__(self) + "\n" + ZbiornikPaliwa.__str__(self) + "\n" + Kolo.__str__(self) + "\n"
+
+sam = Samochod("Audi", "A8", 18, "Michelin", 3.0 , 250, 55)
+sam.setObecnaIlosc(40)
+print(sam)

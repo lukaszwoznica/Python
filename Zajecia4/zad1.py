31f897f66198bc40cb3664ec33fbc33ca52a710e
lukaszwoznica <31970640+lukaszwoznica@users.noreply.github.com>
2018-06-06 13:34:10 +0200

    Update zad1.py

--- Zajecia4/zad1.py
+++ Zajecia4/zad1.py
@@ -7,8 +7,8 @@ napis = """k1:w1
 k2:w2
 k3:w3"""
 
-for lines in lines.splitlines():
+for lines in napis.splitlines():
 	wyrazy = lines.split(":")
 	slownik[wyrazy[0]] = wyrazy[1]
 
-print slownik
+print (slownik)

c714492846eda8332e212908925a18d75547ad7a
lukaszwoznica <31970640+lukaszwoznica@users.noreply.github.com>
2018-06-12 21:01:32 +0200

    Update zad4.py

--- Zajecia6/zad4.py
+++ Zajecia6/zad4.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python
-#encoding: utf-8
-
-import urllib2
+import urllib.request as urllib2
 
 response = urllib2.urlopen('http://python.org/')
 f = open("html_doc.txt", "w")
-f.write(response.read())
+f.write(str(response.read()))
 f.close()

7415f5d045c852b90f11dcc512d3a49b220d3779
lukaszwoznica <31970640+lukaszwoznica@users.noreply.github.com>
2018-06-19 23:39:10 +0200

    Create zad2.py

---
+++ Zajecia5/zad2.py
@@ -0,0 +1,14 @@
+width = int(input())
+f = open("plik.txt")
+count = 0
+res = ''
+for line in f:
+    for char in line:
+        count += 1
+        res += char
+        if count == width:
+            print(res.center(width))
+            count = 0
+            res = ''
+
+f.close()
